aca41ae103a774e24f863565bc18ab6ba3defc48
Adam Rosenblum <AdamR714@gmail.comgit config --global user.emailgit config --global user.email AdamR714@gmail.com>
2017-05-09 21:46:32 -0700

    updated tests

--- test/test-blog.js
+++ test/test-blog.js
@@ -36,8 +36,9 @@ describe('Blog', function() {
 
   it('should add a blog on POST', function() {
     const newBlog = {
-        title: "Order 66", content: "Eliminate All Jedi", author: "Emperor"
+        title: "Order 66", content: "Eliminate All Jedi", author: {firstName: "Bob", lastName: "Emperor"}
     };
+
     return chai.request(app)
       .post('/blog-posts/')
       .send(newBlog)
@@ -45,36 +46,33 @@ describe('Blog', function() {
         res.should.have.status(201);
         res.should.be.json;
         res.body.should.be.a('object');
-        res.body.should.include.keys('id', 'title', 'content' ,'author');
+        res.body.should.include.keys('_id', 'title', 'content' ,'author');
         res.body.title.should.equal(newBlog.title);
         res.body.content.should.equal(newBlog.content);
-        res.body.author.should.equal(newBlog.author);
+        res.body.author.firstName.should.equal(newBlog.author.firstName);
+        res.body.author.lastName.should.equal(newBlog.author.lastName);
+
       });
   });
 
-  it('should update recipes on PUT', function() {
+  it('should update blog on PUT', function() {
     const updateData = {
       title: 'Order 66',
       content: 'Eliminate Yoda',
-      author: "Darth Sidious"
+      author: {firstName: "Darth", lastName: "Sidious"}
     };
 
     return chai.request(app)
       .get('/blog-posts/')
       .then(function(res) {
-        updateData.id = res.body[0].id;
+        updateData._id = res.body[0]._id;
 
         return chai.request(app)
-          .put(`/blog-posts/${updateData.id}`)
+          .put(`/blog-posts/${updateData._id}`)
           .send(updateData)
       })
       .then(function(res) {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        // res.body.should.include.keys('title', 'contnet', 'author');
-        res.body.title.should.equal(updateData.title);
-        res.body.content.should.equal(updateData.content);
-        res.body.author.should.equal(updateData.author);
+        res.should.have.status(204);
       });
   });
 
@@ -89,7 +87,7 @@ describe('Blog', function() {
       .get('/blog-posts/')
       .then(function(res) {
         return chai.request(app)
-          .delete(`/blog-posts/${res.body[0].id}`)
+          .delete(`/blog-posts/${res.body[0]._id}`)
       })
       .then(function(res) {
         res.should.have.status(204);
